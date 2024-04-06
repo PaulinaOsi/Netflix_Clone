6b8b86d8b47c7e3e08bf2ca8349cb0f89ecef46c
Paulina Osikoya <paulinaosikoya@gmail.com>
2024-04-06 12:34:28 +0100

    fix: fixing error in prisma config and loading user data into db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,24 @@
 import NextAuth, { AuthOptions } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import prismadb  from '@/lib/prismadb';
 import { compare } from 'bcrypt';
+
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+import{PrismaAdapter} from '@next-auth/prisma-adapter';
+import prismadb  from '@/lib/prismadb';
 import { Session } from 'inspector';
 
 export const authOptions: AuthOptions = {
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -51,6 +64,7 @@ export const authOptions: AuthOptions = {
         signIn: '/auth'
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt',
     },
